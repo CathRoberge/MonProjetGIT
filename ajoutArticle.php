8d03084e6f016b0494c3225a028cb709e205e606
CathRoberge <e0828186@cmaisonneuve.qc.ca>
2022-01-20 19:41:50 -0500

    ajout de la deconnexion

---
+++ ajoutArticle.php
@@ -0,0 +1,23 @@
+<?php
+//ici j'inclu le fichier des fonctions et connexion à la base de donnée
+require_once("fonctionsDB.php");
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Page d'ajout d'article</title>
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<body>
+    <header>
+        <h1>Ajouter un article</h1>
+    </header>
+    <div class="articles">
+    </div>
+</body>
+
+</html>
